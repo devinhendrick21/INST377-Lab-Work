3f3780d1c9d89931aa00e438438792cd5eb7a8b6
devinhendrick21 <devin.hendrick@gmail.com>
2022-02-17 01:01:06 -0500

    updating tetris

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -59,6 +59,20 @@ function undraw() {
 // make the teromino move down every second
 timerId = setInterval(moveDown, 1000);
 
+// assign functions to keyCodes
+function control(e) {
+    if (e.keyCode === 37) {
+        moveLeft();
+    } else if (e.keyCode === 38) {
+        rotate();
+    } else if (e.keyCode === 39) {
+        moveRight();
+    } else if (e.keyCode === 40) {
+        moveDown();
+    }
+}
+document.addEventListener('keyup', control);
+
 // move down function
 function moveDown() {
     undraw();
@@ -79,6 +93,45 @@ if(current.some(index => squares[currentPosition + index + width].classList.cont
     draw();
     }
 }
+
+//move tetromino left unless blocked
+function moveLeft(){
+    undraw();
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+    
+    if(!isAtLeftEdge) currentPosition -=1;
+    
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition += 1;
+    }
+
+    draw();
+}
+
+// move right
+function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === 9);
+    
+    if(!isAtRightEdge) currentPosition +=1;
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition -= 1;
+    }
+    draw();
+}
+
+// rotate
+function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+        currentRotation = 0;
+    }
+    current = theTetrominos[random][currentRotation];
+    draw();
+}
+
 
 
 
