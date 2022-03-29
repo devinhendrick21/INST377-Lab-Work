4afc88000d275bfb3fe06d488428018396c78ad4
devinhendrick21 <devin.hendrick@gmail.com>
2022-03-29 01:20:35 -0400

    flappy update

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded' , () => {
    let birdLeft = 220;
    let birdBottom = 100;
    let gravity = 2;
+   let isGameOver = false;
 
 
    function startGame () {
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded' , () => {
        bird.style.left = birdLeft + 'px';
    }
 
-   let timerId = setInterval(startGame, 20);
+   let gameTimerId = setInterval(startGame, 20);
 
    function control (e) {
        if(e.keyCode === 32) {
@@ -35,7 +36,7 @@ document.addEventListener('DOMContentLoaded' , () => {
         let randomHeight = Math.random() * 60;
         let obstacleBottom = randomHeight;
         const obstacle = document.createElement('div');
-       obstacle.classList.add('obstacle');
+       if (!isGameOver) obstacle.classList.add('obstacle');
        gameDisplay.appendChild(obstacle);
        obstacle.style.left = obstacleLeft + 'px';
        obstacle.style.bottom = obstacleBottom + 'px';
@@ -48,13 +49,26 @@ document.addEventListener('DOMContentLoaded' , () => {
                clearInterval(timerId);
                gameDisplay.removeChild(obstacle);
            }
+
+           if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+               birdBottom === 0) {
+               gameOver();
+               clearInterval(timerId);
+           }
+
        }
 
        let timerId = setInterval(moveObstacle, 20);
-       setTimeout(generateObstacle, 3000);
+       if (!isGameOver) setTimeout(generateObstacle, 3000);
 
    }
 
    generateObstacle();
+
+   function gameOver () {
+       clearInterval(gameTimerId);
+       isGameOver = true;
+       document.removeEventListener('keyup', control);
+   }
 
 });
