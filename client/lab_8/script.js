59b2934fa1927b1a3e48e92737c3fe25bf9305ba
devinhendrick21 <devin.hendrick@gmail.com>
2022-04-06 21:53:51 -0400

    lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -91,7 +91,7 @@ return map;
     //const arrayFromJson = {data: []}; //TODO
   
     // this if statement is to prevent a race condition on data load
-    
+    if (storedDataArray.length > 0) {
       submit.style.display = 'block';
     
   
